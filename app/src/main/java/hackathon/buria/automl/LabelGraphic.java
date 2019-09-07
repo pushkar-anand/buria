23ac3ea7e50af28b4d5ecbd5d0b3442f2053ed06
Pushkar Anand <anandpushkar088@gmail.com>
2019-09-07 18:59:35 +0530

    Showing only the best confidence guess label now.

--- app/src/main/java/hackathon/buria/automl/LabelGraphic.java
+++ app/src/main/java/hackathon/buria/automl/LabelGraphic.java
@@ -3,19 +3,20 @@ package hackathon.buria.automl;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 import hackathon.buria.camera.GraphicOverlay;
 
-/** Graphic instance for rendering a label within an associated graphic overlay view. */
+/**
+ * Graphic instance for rendering a label within an associated graphic overlay view.
+ */
 public class LabelGraphic extends GraphicOverlay.Graphic {
 
     private final Paint textPaint;
@@ -39,31 +40,70 @@ public class LabelGraphic extends GraphicOverlay.Graphic {
         float x = overlay.getWidth() / 4.0f;
         float y = overlay.getHeight() / 2.0f;
 
-        for (FirebaseVisionImageLabel label : labels) {
-            canvas.drawText(label.getText(), x, y, textPaint);
-            y = y - 62.0f;
+        String maxCLabelStr = null;
+        float maxLabelCValue = Float.MIN_VALUE;
+
+        for(FirebaseVisionImageLabel label: labels) {
+            Log.d("LabelGraphics",
+                    "Label: "+ label.getText() + ", Confidence: " + label.getConfidence());
+            if (label.getConfidence() > maxLabelCValue && label.getConfidence() > 0.5) {
+                maxLabelCValue = label.getConfidence();
+                maxCLabelStr = label.getText();
+            }
+        }
+
+        if (maxCLabelStr != null) {
+            canvas.drawText(maxCLabelStr, x, y, textPaint);
+            //y = y - 62.0f;
 
             if (fullTextView != null) {
 
-                String str = label.getText();
-                if (str.equals("space")) {
-                    fullTextView.append(" ");
-                } else if (str.equals("del")) {
-                    String prev = fullTextView.getText().toString();
-                    String next = removeLastChar(prev);
-                    fullTextView.setText(next);
-
-                } else if(str.equals("nothing")) {
-                    //do nothing
-                } else {
-                    fullTextView.append(str);
+                switch (maxCLabelStr) {
+                    case "space":
+                        fullTextView.append(" ");
+                        break;
+                    case "del":
+                        String prev = fullTextView.getText().toString();
+                        String next = removeLastChar(prev);
+                        fullTextView.setText(next);
+                        break;
+                    case "nothing":
+                        //do nothing
+                        break;
+                    default:
+                        fullTextView.append(maxCLabelStr);
+                        break;
                 }
             }
         }
+
+/*        for (FirebaseVisionImageLabel label : labels) {
+
+            if (label.getConfidence() > 50.0) {
+                canvas.drawText(label.getText(), x, y, textPaint);
+                y = y - 62.0f;
+
+                if (fullTextView != null) {
+
+                    String str = label.getText();
+                    if (str.equals("space")) {
+                        fullTextView.append(" ");
+                    } else if (str.equals("del")) {
+                        String prev = fullTextView.getText().toString();
+                        String next = removeLastChar(prev);
+                        fullTextView.setText(next);
+
+                    } else if (str.equals("nothing")) {
+                        //do nothing
+                    } else {
+                        fullTextView.append(str);
+                    }
+                }
+            }
+        }*/
     }
 
-    private String removeLastChar(@NonNull String str)
-    {
+    private String removeLastChar(@NonNull String str) {
         char lastChar = str.charAt(str.length() - 1);
         int cut = Character.isSurrogate(lastChar) ? 2 : 1;
         return str.substring(0, str.length() - cut);
