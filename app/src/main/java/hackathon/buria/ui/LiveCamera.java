a914952a436de54d8e30609f292d97a6067fc3d4
Pushkar Anand <anandpushkar088@gmail.com>
2019-09-07 14:55:24 +0530

    connected activity to layout.

--- app/src/main/java/hackathon/buria/ui/LiveCamera.java
+++ app/src/main/java/hackathon/buria/ui/LiveCamera.java
@@ -1,25 +1,73 @@
 package hackathon.buria.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback;
 
+import java.io.IOException;
+
 import hackathon.buria.R;
+import hackathon.buria.camera.CameraSource;
+import hackathon.buria.camera.CameraSourcePreview;
+import hackathon.buria.camera.GraphicOverlay;
 import hackathon.buria.helpers.PermissionHelper;
 
 public class LiveCamera extends AppCompatActivity
         implements OnRequestPermissionsResultCallback {
 
     private PermissionHelper ph;
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
+    private GraphicOverlay graphicOverlay;
+    private static final String TAG = "LIVE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.live_camera);
+
         ph = new PermissionHelper(this);
-        ph.requestPermissions();
+
+        if (ph.areAllPermissionsGranted()) {
+            createCameraSource();
+            startCameraSource();
+        } else {
+            ph.requestPermissions();
+        }
+
+        preview = findViewById(R.id.firePreview);
+        graphicOverlay = findViewById(R.id.fireFaceOverlay);
+    }
+
+    private void createCameraSource()
+    {
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this, graphicOverlay);
+        }
+    }
+
+    private void startCameraSource()
+    {
+        if (cameraSource != null) {
+
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null");
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null");
+                }
+                preview.start(cameraSource, graphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                cameraSource.release();
+                cameraSource = null;
+            }
+
+        }
     }
 
 
@@ -30,7 +78,8 @@ public class LiveCamera extends AppCompatActivity
             @NonNull int[] grantResults) {
 
         if (ph.areAllPermissionsGranted()) {
-            //createCameraSource(selectedModel);
+            createCameraSource();
+            startCameraSource();
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
