fcb86b0405fd014f459b4143f0169f39a97f6a98
Pushkar Anand <anandpushkar088@gmail.com>
2019-09-07 14:42:02 +0530

    connected activity to layout.

--- app/src/main/java/hackathon/buria/ui/LiveCamera.java
+++ app/src/main/java/hackathon/buria/ui/LiveCamera.java
@@ -17,7 +17,7 @@ public class LiveCamera extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.live_camera);
         ph = new PermissionHelper(this);
         ph.requestPermissions();
     }
