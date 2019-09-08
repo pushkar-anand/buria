221c28ccaf9172949c8df681b9cb75123fbf3f84
Pushkar Anand <anandpushkar088@gmail.com>
2019-09-08 09:35:56 +0530

    added selection activity.

---
+++ app/src/main/java/hackathon/buria/ui/LauncherActivity.java
@@ -0,0 +1,16 @@
+package hackathon.buria.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hackathon.buria.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
